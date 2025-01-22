68d2fa477f30ea905a3deb76b9daae4ef426fbe4
Bryan Ross Roque <eliasdeeroque@gmail.com>
2025-01-21 19:53:53 -0500

    Added lab1a

--- lab1a.py
+++ lab1a.py
@@ -5,6 +5,8 @@
 # these lines are ignored by the python interpreter even if
 # they contain code. The very first line is called a Shebang line, 
 # it is used to tell the system which interpreter to 
-# use(python2, python3, bash, etc).
+# use(python2, python3, bash, etc). 
 
 print('Hello world')
+
+
